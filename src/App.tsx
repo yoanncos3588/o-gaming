ec8569d0e82fe777a4e5c3d97a34900c4f917367
YoannCos <yoanncos@gmail.com>
2023-08-21 11:22:08 +0200

    config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
-  return (
-    <>
-      <div className='text-2xl'>hello world</div>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
-    </>
-  )
+    return (
+        <>
+            <div className="text-2xl">hello world</div>
+            <button className="btn btn-info">Info</button>
+            <button className="btn btn-success">Success</button>
+            <button className="btn btn-warning">Warning</button>
+            <button className="btn btn-error">Error</button>
+        </>
+    );
 }
 
-export default App
+export default App;

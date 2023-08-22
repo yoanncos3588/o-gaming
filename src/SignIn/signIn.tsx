d68dffff21387bdb8d4d53429389213912358466
Raphael burnier <burnierraphael5@gmail.com>
2023-08-22 14:45:58 +0200

    sign up

--- src/SignIn/signIn.tsx
+++ src/SignIn/signIn.tsx
@@ -2,81 +2,84 @@ import './signIn.css';
 
 function signIn() {
     return (
-        <main className="main">
-            <div className="container">
-                <span className="title">
-                    <h1 className="text-4xl text-start">Sign up</h1>
-                </span>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">username</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="enter a username"
-                        className="input input-bordered w-full max-w-xs"
-                    />
+        <section className="h-full gradient-form bg-gray-200 md:h-screen">
+            <div className="container py-12 px-6 h-full">
+                <div className=" flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                    <div className="">
+                        <div className="block bg-white shadow-lg rounded-lg">
+                            <div className="lg:flex lg:flex-wrap g-0">
+                                <div className="px-4 md:px-0">
+                                    <div className="md:p-12 md:mx-6">
+                                        <div className="text-center">
+                                            <h4 className="text-2xl font-semibold mt-1 mb-8 pt-2">
+                                                sign up
+                                            </h4>
+                                        </div>
+                                        <form>
+                                            <p className="mb-4">
+                                                Please Sign Up if you do not
+                                                have an account
+                                            </p>
+                                            <div className="mb-4">
+                                                <input
+                                                    type="text"
+                                                    className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                                    placeholder="username"
+                                                    name="userEmail"
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <input
+                                                    type="email"
+                                                    className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                                    placeholder="email"
+                                                    name="pin"
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <input
+                                                    type="password"
+                                                    className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                                    placeholder="Password"
+                                                    name="pin"
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <input
+                                                    type="password"
+                                                    className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                                    placeholder="confirm Password"
+                                                    name="pin"
+                                                />
+                                            </div>
+                                            <div className="text-center pt-1 mb-12 pb-1">
+                                                <button
+                                                    className="bg-green inline-block px-6 py-2.5 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                                                    type="button"
+                                                >
+                                                    Sign Up
+                                                </button>
+                                            </div>
+                                            <div className="flex items-center justify-between pb-6">
+                                                <p className="mb-0 mr-2">
+                                                    Do you have an account?
+                                                </p>
+                                                <button
+                                                    type="button"
+                                                    className="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                                                >
+                                                    Log In
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">email</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="add an email"
-                        className="input input-bordered w-full max-w-xs"
-                    />
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">password</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="enter password"
-                        className="input input-bordered w-full max-w-xs"
-                    />
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">confirm password</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="comfirm your password"
-                        className="input input-bordered w-full max-w-xs"
-                    />
-                </div>
-
-                <div className="join">
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="options"
-                        aria-label="developer"
-                    />
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="options"
-                        aria-label="Gamer"
-                    />
-                </div>
-                <div className="sign-up">
-                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
-                        sign up
-                    </button>
-                </div>
-
-                <span className="footer-title">already have an password</span>
-
-                <a className="link link-hover text-xl">Log in</a>
             </div>
-        </main>
+        </section>
     );
 }
 

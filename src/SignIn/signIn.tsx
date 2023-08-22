b6f37a0b75f2d28c7c5cae1061c6189835a21853
Raphael burnier <burnierraphael5@gmail.com>
2023-08-22 15:32:43 +0200

    sign up (terminer)

--- src/SignIn/signIn.tsx
+++ src/SignIn/signIn.tsx
@@ -4,7 +4,7 @@ function signIn() {
     return (
         <section className="h-full gradient-form bg-gray-200 md:h-screen">
             <div className="container py-12 px-6 h-full">
-                <div className=" flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                <div className=" flex justify-center items-center flex-col h-full g-6 text-gray-800">
                     <div className="">
                         <div className="block bg-white shadow-lg rounded-lg">
                             <div className="lg:flex lg:flex-wrap g-0">
@@ -52,21 +52,35 @@ function signIn() {
                                                     name="pin"
                                                 />
                                             </div>
-                                            <div className="text-center pt-1 mb-12 pb-1">
+                                            <div className="join w-80 flex justify-center">
+                                                <input
+                                                    className="join-horizontal btn bg-white  text-black hover:text-white focus:bg-black border-gray-300"
+                                                    type="radio"
+                                                    name="options"
+                                                    aria-label="Developpeur"
+                                                />
+                                                <input
+                                                    className="join-horizontal btn  bg-white mb-2 text-black hover:text-white border-gray-300"
+                                                    type="radio"
+                                                    name="options"
+                                                    aria-label="Gamer"
+                                                />
+                                            </div>
+                                            <div className="text-center pt-1 mb-5 pb-1">
                                                 <button
-                                                    className="bg-green inline-block px-6 py-2.5 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                                                    className=" mt-4 inline-block px-6 py-2.5 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
                                                     type="button"
                                                 >
                                                     Sign Up
                                                 </button>
                                             </div>
-                                            <div className="flex items-center justify-between pb-6">
+                                            <div className="flex items-center justify-between pb-6 border-t-2 border-gray-300 pt-4">
                                                 <p className="mb-0 mr-2">
                                                     Do you have an account?
                                                 </p>
                                                 <button
                                                     type="button"
-                                                    className="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                                                    className="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-green-700 hover:text-white focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                                                 >
                                                     Log In
                                                 </button>

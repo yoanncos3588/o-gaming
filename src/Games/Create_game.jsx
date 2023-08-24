7fac6d9dfa7a85b88f03370ff398b8bdeb045e02
Raphael burnier <burnierraphael5@gmail.com>
2023-08-24 13:24:16 +0200

    create game presque bon

--- src/Games/Create_game.jsx
+++ src/Games/Create_game.jsx
@@ -2,7 +2,7 @@ function Create_game() {
     return (
         <main>
             <div className="flex justify-center items-center">
-                <div className=" min-w-700 max-w-1000 bg-neutral p-8 rounded-lg shadow-lg">
+                <div className=" w-full md:w-1/2 xl:w-3/5 bg-neutral p-8 rounded-lg shadow-lg m-2">
                     <h2 className="text-2xl font-semibold text-white mb-4">
                         Create a game
                     </h2>
@@ -12,44 +12,87 @@ function Create_game() {
                                 htmlFor="name"
                                 className="block text-white text-sm font-medium mb-2"
                             >
-                                Nom
+                                title
                             </label>
                             <input
                                 type="text"
                                 id="name"
                                 name="name"
                                 className="w-full px-3 py-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring focus:border-blue-300"
-                                placeholder="Entrez votre nom"
+                                placeholder="add a title"
                             />
                         </div>
-                        <div className="mb-4">
-                            <label
-                                htmlFor="email"
-                                className="block text-white text-sm font-medium mb-2"
-                            >
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                className="w-full px-3 py-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring focus:border-blue-300"
-                                placeholder="Entrez votre email"
-                            />
+                        <div className="flex">
+                            <button className="btn btn-info justify-end">
+                                add
+                            </button>
                         </div>
+
+                        <section className="mb-4 flex flex-wrap w-3/6">
+                            <div className="form-control w-1/2 mr-3">
+                                <label className="cursor-pointer label ">
+                                    <span className="label-text">action</span>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox checkbox-info"
+                                    />
+                                </label>
+                            </div>
+                            <div className="form-control w-1/2 mr-3">
+                                <label className="cursor-pointer label ">
+                                    <span className="label-text">
+                                        Simulation
+                                    </span>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox checkbox-info"
+                                    />
+                                </label>
+                            </div>
+                            <div className="form-control w-1/4 mr-3">
+                                <label className="cursor-pointer label ">
+                                    <span className="label-text">aventure</span>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox checkbox-info"
+                                    />
+                                </label>
+                            </div>
+                            <div className="form-control w-1/2 mr-3">
+                                <label className="cursor-pointer label ">
+                                    <span className="label-text">compte</span>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox checkbox-info"
+                                    />
+                                </label>
+                            </div>
+                            <div className="form-control w-2/4 mr-3">
+                                <label className="cursor-pointer label ">
+                                    <span className="label-text">
+                                        interface
+                                    </span>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox checkbox-info"
+                                    />
+                                </label>
+                            </div>
+                        </section>
+
                         <div className="mb-4">
                             <label
                                 htmlFor="message"
                                 className="block text-white text-sm font-medium mb-2"
                             >
-                                Message
+                                description
                             </label>
                             <textarea
                                 id="message"
                                 name="message"
                                 rows="4"
                                 className="w-full px-3 py-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring focus:border-blue-300"
-                                placeholder="Entrez votre message"
+                                placeholder="add an description"
                             ></textarea>
                         </div>
                         <button

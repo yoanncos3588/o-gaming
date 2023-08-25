02f0ea1df1cf99450de43661ce004f43ce1691dc
YoannCos <yoanncos@gmail.com>
2023-08-25 10:40:12 +0200

    add library toastify

--- src/Example.jsx
+++ src/Example.jsx
@@ -1,4 +1,6 @@
 import ContentContainer from './ContentContainer';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 /**
  * Example of page using template contentcontainer
@@ -6,11 +8,16 @@ import ContentContainer from './ContentContainer';
  */
 
 function Example() {
+    const notify = () => toast('Wow so easy!');
     return (
         <ContentContainer
             SidebarLeft={<div>Side content components or html</div>}
         >
-            <div>Main content components or html</div>
+            <div>
+                {' '}
+                <button onClick={notify}>Notify!</button>
+                <ToastContainer />
+            </div>
         </ContentContainer>
     );
 }

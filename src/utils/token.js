f5581f208580d112fc517299af316ec30dc5f8a4
YoannCos <yoanncos@gmail.com>
2023-08-30 18:54:34 +0200

    add token expiration verification and refactor token functions

---
+++ src/utils/token.js
@@ -0,0 +1,74 @@
+import jwt_decode from 'jwt-decode';
+import store from './../store';
+import { logout } from './../store/reducers/user';
+
+/**
+ * Retrieve user info frome token body
+ * @param {string} token
+ * @returns {object} {userId, role}
+ */
+export const decodeToken = (token) => {
+    if (token || token !== undefined) {
+        const decoded = jwt_decode(token);
+        const userData = {
+            userId: decoded.userId,
+            username: decoded.username,
+            role: decoded.role,
+        };
+        return userData;
+    } else {
+        return null;
+    }
+};
+
+/**
+ * Save user infos and login info in localstorage
+ * @param {object} userData
+ * @param {string} token
+ */
+export const setUserTokens = (userData, token) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem('token', JSON.stringify(token));
+    localStorage.setItem('token_exp', JSON.stringify(getTokenExpirationDate()));
+};
+
+/**
+ * Delete user's tokens and info from localstorage
+ */
+export const removeUserTokens = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('token_exp');
+};
+
+/**
+ * Get expiration date from user token
+ * @returns {number || null} expiration time or null if no token found
+ */
+export const getTokenExpirationDate = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        const decoded = jwt_decode(token);
+        return decoded.exp;
+    } else {
+        return null;
+    }
+};
+
+/**
+ * Check if user token from local storage is still valid
+ * @returns {boolean}
+ */
+export const isTokenExpired = () => {
+    const exp = localStorage.getItem('token_exp');
+    if (!exp) {
+        return true;
+    }
+    if (exp * 1000 < new Date().getTime()) {
+        //disconnect user
+        store.dispatch(logout());
+        return true;
+    } else {
+        return false;
+    }
+};
--- src/utils/userStatus.js
+++ src/utils/userStatus.js
@@ -1,11 +1,21 @@
+import { isTokenExpired } from './token';
+
+/**
+ * check if user is still connected by checking token expiration time and store for more security
+ * @param {*} userData
+ * @returns
+ */
 export const isLoggedIn = (userData) => {
+    if (isTokenExpired()) {
+        return false;
+    }
     return userData?.userId ? true : false;
 };
 
 export const isDeveloper = (userData) => {
-    return userData?.role === 'developer' && isLoggedIn();
+    return userData?.role === 'developer' && isLoggedIn(userData);
 };
 
 export const isGamer = (userData) => {
-    return userData?.role === 'gamer' && isLoggedIn();
+    return userData?.role === 'gamer' && isLoggedIn(userData);
 };

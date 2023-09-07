263e3febbc70d0574031c549923112be54d194e3
YoannCos <yoanncos@gmail.com>
2023-09-07 13:22:42 +0200

    fix userData in canDelete sidebar + suggestion loaded condition in suggestion component

--- src/components/Issue/Suggestion.jsx
+++ src/components/Issue/Suggestion.jsx
@@ -71,7 +71,7 @@ const Suggestion = () => {
 
     return (
         <>
-            {!game && !suggestion ? (
+            {!game || !suggestion ? (
                 <Loading />
             ) : (
                 <ContentContainer
